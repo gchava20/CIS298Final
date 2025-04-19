cf52ba0cba8421f8686a0fa114e87b8307ed245c
RobertKhzouz <rckhzouz@gmail.com>
2025-04-19 18:14:40 -0400

    Export to CSV option after view

--- driver.py
+++ driver.py
@@ -1,8 +1,9 @@
 from api_master import view_symbol
+from calculations import export_to_csv
 
 def main():
     while True:
-        company = input("Which company would you like to view? If you want to quit the program, type exit").strip().upper()
+        company = input("Which company would you like to view? If you want to quit the program, type exit: ").strip().upper()
         if company.lower() == 'exit':
             break
 
@@ -12,7 +13,12 @@ def main():
             break
         elif selection == "1":
             try:
-                view_symbol(company)
+                df = view_symbol(company, ret=True)
+                if export := input("Would you like to export to CSV? (Y/N): ") == "Y":
+                    export_to_csv(df)
+                else:
+                    continue
+
             except Exception as e:
                 print(f"Error fetching data for {company}: {e}")
         else:
