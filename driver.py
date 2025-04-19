01b0bfdb43cc8058d2d72e280fb69a1ce5ab883c
RobertKhzouz <rckhzouz@gmail.com>
2025-04-19 19:39:05 -0400

    Selections for Industry & Sector

--- driver.py
+++ driver.py
@@ -1,4 +1,4 @@
-from api_master import view_symbol
+from api_master import view_symbol, view_sector, view_industry
 from calculations import export_to_csv
 
 def main():
@@ -7,7 +7,7 @@ def main():
         if company.lower() == 'exit':
             break
 
-        selection = input("Select which action you'd like to perform: (1) VIEW SYMBOL, (-1) EXIT : ").strip()
+        selection = input("Select which action you'd like to perform:\n(1) VIEW SYMBOL\n(2) VIEW SECTOR\n(3) VIEW INDUSTRY\n(-1) EXIT\n").strip()
         
         if selection == "-1":
             break
@@ -21,6 +21,13 @@ def main():
 
             except Exception as e:
                 print(f"Error fetching data for {company}: {e}")
+
+        elif selection == "2":
+            view_sector(company)
+
+        elif selection == "3":
+            view_industry(company)
+
         else:
             print("Invalid selection. Try again.")
 
